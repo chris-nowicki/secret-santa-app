e7ae388d8f721f8de5e7282680adfa9bf5fa33ae
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-12-01 22:17:32 -0500

    My First Commit

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+// https://prettier.io/docs/en/options.html
+/** @type {import('prettier').RequiredOptions} */
+module.exports = {
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'always',
+  overrides: [
+    {
+      files: 'Routes.*',
+      options: {
+        printWidth: 999,
+      },
+    },
+  ],
+  tailwindConfig: 'tailwind.config.ts',
+  plugins: [require('prettier-plugin-tailwindcss')],
+}
